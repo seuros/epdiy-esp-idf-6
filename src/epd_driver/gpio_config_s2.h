538bf8ce95f99472c309b23af1500280bce1b73d
Martin Fasani <fasani@skygate.de>
2022-01-31 16:35:34 +0100

    #1 IOs

--- src/epd_driver/gpio_config_s2.h
+++ src/epd_driver/gpio_config_s2.h
@@ -1,19 +1,19 @@
 
-#define D7 GPIO_NUM_42
-#define D6 GPIO_NUM_21
-#define D5 GPIO_NUM_35
-#define D4 GPIO_NUM_2
-#define D3 GPIO_NUM_19
-#define D2 GPIO_NUM_37
-#define D1 GPIO_NUM_36
+#define D7 GPIO_NUM_40
+#define D6 GPIO_NUM_39
+#define D5 GPIO_NUM_38
+#define D4 GPIO_NUM_37
+#define D3 GPIO_NUM_36
+#define D2 GPIO_NUM_35
+#define D1 GPIO_NUM_34
 #define D0 GPIO_NUM_33
 
-#define CFG_DATA GPIO_NUM_38
+#define CFG_DATA GPIO_NUM_41
 #define CFG_CLK GPIO_NUM_18
 #define CFG_STR GPIO_NUM_19
 /* Control Lines */
-#define CKV GPIO_NUM_45
-#define STH GPIO_NUM_20
+#define CKV GPIO_NUM_42
+#define STH GPIO_NUM_45
 
 #define V4_LATCH_ENABLE GPIO_NUM_14
 /* Edges */
