93ffa55591f141c2b9a2e11bd6ec60b1e9e33da1
martinberlin <martin@luckycloud.de>
2022-02-16 11:33:53 +0100

    Add also ED047TC2 waveform

--- builtin_waveforms.c
+++ builtin_waveforms.c
@@ -2,6 +2,7 @@
 
 
 #include "waveforms/epdiy_ED047TC1.h"
+#include "waveforms/epdiy_ED047TC2.h"
 #include "waveforms/epdiy_ED060SC4.h"
 #include "waveforms/epdiy_ED060XC3.h"
 #include "waveforms/epdiy_ED060SCT.h"
